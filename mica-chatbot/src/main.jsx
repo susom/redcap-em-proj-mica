3cdd4b3f9ef9bce5c06e57308f1280ff152f2686
Jordan Schultz <jmschult@stanford.edu>
2024-07-08 16:37:00 -0700

    Added user info to dexie for caching

--- mica-chatbot/src/main.jsx
+++ mica-chatbot/src/main.jsx
@@ -8,6 +8,7 @@ import '@mantine/core/styles.css';
 import '@mantine/carousel/styles.css';
 import { MantineProvider } from '@mantine/core';
 import {AppRouter} from "./components/appRouter/appRouter.jsx";
+// Strict mode will double invoke lifecycle methods for dev purposes :
 
 ReactDOM.createRoot(document.getElementById('chatbot_ui_container')).render(
   <React.StrictMode>
