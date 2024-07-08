5341cbe25ffcaf129212831ff25d276748ea9125
Jordan Schultz <jmschult@stanford.edu>
2024-07-08 16:35:50 -0700

    Added useAuth hook with login and logout functionality

---
+++ mica-chatbot/src/Hooks/useAuth.jsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import {user_info} from "../components/database/dexie.js";
+
+const authContext = React.createContext();
+
+function useAuth() {
+    const [authed, setAuthed] = React.useState(false);
+
+    const checkUserCache = async (key) => {
+        return await user_info.current_user.get(key) ?? null;
+    }
+
+    return {
+        authed,
+        login(name, email) {
+            return new Promise(async (resolve, reject) => {
+                try {
+                    const user = await checkUserCache(1)
+                    if(user){
+                        setAuthed(true);
+                        resolve();
+                    } else { //Attempt logging in user via REST
+                        const mica = mica_jsmo_module
+                        if(mica) {
+                            let result = await mica_jsmo_module.login({
+                                'name': name,
+                                'email': email
+                            }, resolve, reject)
+                        } else {
+                            console.error('MICA EM is not injected, cannot execute function login')
+                            reject();
+                        }
+                    }
+                } catch (error) {
+                    console.error('Login failed: ', error)
+                    reject(error);
+                }
+
+            });
+        },
+        logout() {
+            return new Promise((res) => {
+                setAuthed(false);
+                res();
+            });
+        },
+    };
+}
+
+export function AuthProvider({ children }) {
+    const auth = useAuth();
+
+    return <authContext.Provider value={auth}>{children}</authContext.Provider>;
+}
+
+export default function AuthConsumer() {
+    return React.useContext(authContext);
+}
